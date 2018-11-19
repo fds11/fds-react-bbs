f6b753217cd00e10d86fc13d6f656067d38a7d69
Kim Seungha <seungha.me@gmail.com>
2018-11-19 16:15:02 +0900

    회원가입/로그인

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 import LoginForm from './components/LoginForm'
+import RegisterForm from './components/RegisterForm'
+
+// 로그인 폼에 회원가입 버튼 만들기
+// 회원가입 버튼 클릭하면 회원가입 폼 보여주기
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    // page === 'login' -> 로그인 페이지
+    // page === 'register' -> 회원가입 페이지
+    this.state = {
+      page: 'login'
+    }
+  }
+
+  handleRegisterPage() {
+    this.setState({
+      page: 'register'
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <LoginForm />
+        {this.state.page === 'login' ? (
+          <LoginForm onRegister={() => this.handleRegisterPage()} />
+        ) : this.state.page === 'register' ? (
+          <RegisterForm />
+        ) : null}
       </div>
     );
   }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import api from '../api';
 
 export default class LoginForm extends React.Component {
-    render() {
-        return (
-            <form>
-                <h1>로그인</h1>
-            </form>
-        )
-    }
+  async handleSubmit(e) {
+    e.preventDefault()
+    const username = e.target.elements.username.value
+    const password = e.target.elements.password.value
+    const res = await api.post('/users/login', {
+      username,
+      password
+    })
+    localStorage.setItem('token', res.data.token)
+    // TODO: 게시글 목록 보여주기
+  }
+  render() {
+    const {onRegister} = this.props
+    return (
+      <React.Fragment>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <h1>로그인</h1>
+          <input type="text" name="username" />
+          <input type="password" name="password" />
+
+          <button>로그인</button>
+        </form>
+        <button onClick={() => onRegister()}>회원 가입</button>
+      </React.Fragment>
+    )
+  }
 }
