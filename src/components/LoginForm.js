b6273021a6b13b4387df42492d48baf1e1d5bde7
Kim Seungha <seungha.me@gmail.com>
2018-11-19 15:04:38 +0900

    LoginForm

---
+++ src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class LoginForm extends React.Component {
+    render() {
+        return (
+            <form>
+                <h1>로그인</h1>
+            </form>
+        )
+    }
+}
