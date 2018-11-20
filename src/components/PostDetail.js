62a9cdab574cdb12b61645f33c9ee5ee61d48a1e
Kim Seungha <seungha.me@gmail.com>
2018-11-20 15:42:23 +0900

    게시물 내용 표시 기능

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
+import api from '../api';
 
 export default class PostDetail extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      body: '',
+      title: ''
+    }
+  }
+  
+  async componentDidMount() {
+    const {data: {title, body}} = await api.get(`/posts/${this.props.postId}`)
+    this.setState({
+      title,
+      body
+    })
+  }
+  
   render() {
     const {postId} = this.props
+    const {title, body} = this.state
+
     return (
       <div>
         <h1>게시물</h1>
-        {postId}
+        <div>{title}</div>
+        <div>{body}</div>
       </div>
     )
   }
