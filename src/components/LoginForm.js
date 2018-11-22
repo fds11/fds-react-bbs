9939fdd5ce1c0a6240ec4a331207328c4e60e194
Kim Seungha <seungha.me@gmail.com>
2018-11-22 15:11:52 +0900

    로그인 기능 추가

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import api from '../api';
+import {UserConsumer} from '../contexts/UserContext'
 
 export default class LoginForm extends React.Component {
   constructor(props) {
@@ -9,30 +9,28 @@ export default class LoginForm extends React.Component {
     this.passwordRef = React.createRef()
   }
   
-  async handleSubmit(e) {
-    e.preventDefault()
-    const username = this.usernameRef.current.value
-    const password = e.target.elements.password.value
-    const res = await api.post('/users/login', {
-      username,
-      password
-    })
-    localStorage.setItem('token', res.data.token)
-    // TODO: 게시글 목록 보여주기
-  }
   render() {
     const {onRegister} = this.props
     return (
-      <React.Fragment>
-        <form onSubmit={e => this.handleSubmit(e)}>
-          <h1>로그인</h1>
-          <input ref={this.usernameRef} type="text" name="username" />
-          <input ref={this.passwordRef} type="password" name="password" />
+      <UserConsumer>
+        {({login}) => (
+          <React.Fragment>
+            <form onSubmit={e => {
+              e.preventDefault()
+              const username = e.target.elements.username.value
+              const password = e.target.elements.password.value
+              login(username, password)
+            }}>
+              <h1>로그인</h1>
+              <input ref={this.usernameRef} type="text" name="username" />
+              <input ref={this.passwordRef} type="password" name="password" />
 
-          <button>로그인</button>
-        </form>
-        <button onClick={() => onRegister()}>회원 가입</button>
-      </React.Fragment>
+              <button>로그인</button>
+            </form>
+            <button onClick={() => onRegister()}>회원 가입</button>
+          </React.Fragment>
+        )}
+      </UserConsumer>
     )
   }
 }
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react'
 
+import api from '../api'
+
 const {Provider, Consumer} = React.createContext()
 
 export default class UserProvider extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      id: null,
+      username: null
+    }
+  }
+
+  async componentDidMount() {
+    if (localStorage.getItem('token')) {
+      await this.refreshUser()
+    }
+  }
+  
+  async login(username, password) {
+    const res = await api.post('/users/login', {
+      username,
+      password
+    })
+    localStorage.setItem('token', res.data.token)
+    await this.refreshUser()
+    // TODO: 게시글 목록 보여주기
+  }
+
+  async refreshUser() {
+    const res2 = await api.get('/me')
+    this.setState({
+      id: res2.data.id,
+      username: res2.data.username
+    })
+  }
+
   render() {
+    const value = {
+      username: this.state.username,
+      id: this.state.id,
+      login: this.login.bind(this)
+    }
     return (
-      <Provider value={{username: 'fast'}}>{this.props.children}</Provider>
+      <Provider value={value}>{this.props.children}</Provider>
     )
   }
 }
